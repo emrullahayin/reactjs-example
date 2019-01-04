fb6a96685ad0d6c3d178846f34878352b9636ffe
Emrullah Ayın <e.ayin@jollytur.com>
2019-01-04 12:28:57 +0300

    package.json update

--- src/components/News.js
+++ src/components/News.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import NewsItem from './NewsItem';
 
 class News extends Component {
+  static propTypes = {
+    news: PropTypes.array.isRequired,
+    name: PropTypes.oneOfType([
+      PropTypes.number,
+      PropTypes.string
+    ])
+  };
   render() {
     const elements = this.props.news.map(news =>
       <NewsItem
         key={news.id}
-        title={news.title}
-        description={news.description} />
+        newsData={news} />
     );
     return (
       <div>
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 
-
 class NewsItem extends Component {
+  // newsData: PropTypes.shape({
+  //   id: PropTypes.number,
+  //   title: PropTypes.string,
+  //   description: PropTypes.string
+  // }),
   render() {
-    const { title, description } = this.props;
+    const { title, description} = this.props.newsData;
     return (
       <div>
-        <h1 className="title">Hello {title}</h1>
+        <h1 className="title">{title}</h1>
         <p>{description}</p>
       </div>
     )
