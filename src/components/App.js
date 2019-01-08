11a42e1dc5b43adf04502a57135cd5926b156f27
Emrullah <emrullahayin@gmail.com>
2019-01-09 00:40:43 +0300

    Todo List App

--- src/components/App.js
+++ src/components/App.js
@@ -1,36 +1,71 @@
-import React, { Component } from 'react';
-import News from './News';
+import React, { Component } from 'react'
 
-const news = [{
-  id: 1,
-  title: "title 1",
-  description: "description 1"
-}, {
-  id: 2,
-  title: "title 2",
-  description: "description 2"
-}, {
-  id: 3,
-  title: "title 3",
-  description: "description 3"
-}];
-
-class App extends Component {
-  addNews(){
-    console.log('AddNews Eklendi');
-    news.push({
-      id: 4,
-      title: "title 4",
-      description: "description 4"
+export class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+      text: "",
+      errorValid: false
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleAddItem = this.handleAddItem.bind(this);
+    this.handleDeleteItem = this.handleDeleteItem.bind(this);
+  };
+  handleChange(e) {
+    this.setState({
+      text: e.target.value
     });
   };
+  handleAddItem() {
+    let { text, data, errorValid } = this.state;
+    debugger;
+    if (text.length) {
+      if (data.length) {
+        for (var i = 0; i < data.length; i++) {
+          if (data[i] === text) {
+            errorValid = true;
+          }
+        }
+        if (errorValid === false) data.push(text)
+      } else {
+        data.push(text)
+      }
+      this.setState(
+        this.state
+      )
+    }
+  }
+  handleDeleteItem(item) {
+    for (var i = 0; i < this.state.data.length; i++) {
+      if (this.state.data[i] === item) {
+        delete this.state.data[i]
+      }
+    }
+    this.setState(
+      this.state
+    )
+  }
   render() {
+    let { data, errorValid } = this.state;
     return (
-      <div className="App">
-        <News news={news} addNews={this.addNews} />
+      <div>
+        <input
+          className={errorValid === true ? "error" : ""}
+          type="search"
+          placeholder="Ekle"
+          onChange={this.handleChange} />
+        <button
+          type="button"
+          onClick={this.handleAddItem}>Add</button>
+        <ul>
+          {data.map((item, index) => {
+            return <li key={index} onClick={this.handleDeleteItem.bind(this, item)}>{item}<del>Delete</del></li>
+          })}
+        </ul>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
